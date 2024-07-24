c171a42213ead6b77f2000f58ab41170b5f3b7fc
ajbecks <aaron_beckles@hotmail.co.uk>
2024-07-24 13:36:05 +0100

    helmet

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 import helmet from 'helmet'
 
-app.use(helmet,({X-Powered-by: false}))
+app.use(helmet())
 
 app.get('/', (req, res) => {
   res.send('Say hello to my little friend...')
